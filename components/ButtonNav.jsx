db1aa61f36b07c34074f146761ec0fd7dceb8dae
zhafranth <zhafranthGgmail.com>
2021-10-23 02:13:07 +0700

    fix link

--- components/ButtonNav.jsx
+++ components/ButtonNav.jsx
@@ -1,13 +1,12 @@
-import React from "react";
+import Link from "next/link";
 
 const ButtonNav = ({ children }) => {
   return (
-    <a
-      href="/"
-      className="relative text-lg text-black hover:text-white font-light inline-block py-1 px-6 bg-transparent transition-all rounded-md after:absolute after:bg-primary after:w-0 hover:after:w-full after:transition-all after:duration-300 after:h-full after:top-0 after:left-0 after:z-10 after:bg-opacity-50 after:rounded-md"
-    >
-      <span className="relative z-30">{children}</span>
-    </a>
+    <Link href="/">
+      <a className="relative text-lg text-black hover:text-white font-light inline-block py-1 px-6 bg-transparent transition-all rounded-md after:absolute after:bg-primary after:w-0 hover:after:w-full after:transition-all after:duration-300 after:h-full after:top-0 after:left-0 after:z-10 after:bg-opacity-50 after:rounded-md">
+        <span className="relative z-30">{children}</span>
+      </a>
+    </Link>
   );
 };
 
